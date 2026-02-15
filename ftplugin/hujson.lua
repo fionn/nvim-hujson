b61bcddb5667be55351327229be41d4c0157792d
Fionn Fitzmaurice <fionn@github.com>
2026-02-15 19:30:43 +0800

    Only set formatprg if hujsonfmt is installed
    This also lets us simplify the logic around if we want to format on
    write, now we just assume we do if formatprg is set.

--- ftplugin/hujson.lua
+++ ftplugin/hujson.lua
@@ -8,21 +8,22 @@ vim.b.did_ftplugin_hujson = true
 vim.cmd("runtime! ftplugin/jsonc.vim")
 vim.cmd("runtime! indent/jsonc.vim")
 
-vim.bo.formatprg = "hujsonfmt"
-vim.bo.expandtab = false
-
 -- We anyway use the json parser for jsonc, this replicates the logic for
 -- hujson.
 vim.treesitter.language.register("json", "hujson")
+
+vim.bo.expandtab = false
+
+if vim.fn.executable("hujsonfmt") == 1 then
+    vim.bo.formatprg = "hujsonfmt"
+end
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     group = vim.api.nvim_create_augroup("HuJSONFormat", {clear = true}),
     desc = "Format HuJSON",
     buffer = 0,
     callback = function()
-        -- In case the formatprg is overridden to include flags or something,
-        -- take only the head.
-        if vim.fn.executable(vim.fn.split(vim.bo.formatprg, " ")[1]) == 1 then
+        if vim.bo.formatprg ~= "" then
             local view = vim.fn.winsaveview()
             vim.cmd("silent keepjumps normal! gggqG")
             vim.fn.winrestview(view)
